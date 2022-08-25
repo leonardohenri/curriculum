c1328b0ca24cff859be572a3a986f2b696a3dc54
leonardo henrique da silva <leonardohenri2015@outlook.com>
2022-08-25 09:38:13 -0300

    colocando margin na grid

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -8,10 +8,12 @@ import { GridFour } from '../../components/four';
 
 export const Home = () =>{
 return (<div className='page'>
+  <div className='styles-grid'>
   <GridOne />
   <GridTwo />
   <GridThree />
   <GridFour/>
+  </div>
   </div>
 );
 }
